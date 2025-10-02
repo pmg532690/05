36b37e61e26b3c0c6371fc343c433086a9d75327
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-10-02 15:31:29 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,16 @@
 int main(int argc, char *argv[])
 {
     int x;
-    printf("Input an integar:");
+    int i;
+    int sum = 0;
+    
+    printf("Input a  integar:");
     scanf("%d", &x);
     
-    if ( x > 0 )
-       printf("절댓값은 %i 입니다.\n", x);
-       
-    else 
-       printf("절댓값은 %d 입니다.\n", -x);
+    
+    for(i=1;i<=x;i++)
+                 sum = sum +i;
+    printf("sum is %i\n", sum);
        
   system("PAUSE");	
   return 0;
